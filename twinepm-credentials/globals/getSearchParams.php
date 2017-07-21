2aebe9b8d98614d88c23d1858f8f0aef1ed4076b
furkle <furkle@furkleindustries.com>
2017-07-21 18:26:02 -0400

    Adding CI.

--- twinepm-credentials/globals/getSearchParams.php
+++ twinepm-credentials/globals/getSearchParams.php
@@ -1,254 +1,233 @@
 <?php
 require_once __DIR__ . "/getSearchDefaults.php";
-function getSearchParams() {
-	$defaults = getSearchDefaults();
+function getSearchParams($source) {
+    $defaults = getSearchDefaults();
+    $yesStrict = true;
+    $response = array("status" => 200);
 
-	$response = array("status" => 200);
+    $query = isset($source["query"]) ? strtolower($source["query"]) : null;
+    if ($query === null) {
+        $response["status"] = 400;
+        $response["error"] = "The query argument was not provided.";
+        return $response;
+    }
 
-	$type = isset($_GET["type"]) ? $_GET["type"] : "";
-	if (!$type) {
-		$response["status"] = 400;
-		$response["error"] = "The type argument was not provided.";
-		return $response;
-	}
+    $type = isset($source["type"]) ? strtolower($source["type"]) : "";
+    if (!$type) {
+        $response["status"] = 400;
+        $response["error"] = "The type argument was not provided.";
+        return $response;
+    } else if (!in_array($type, $defaults["type"], $yesStrict)) {
+        $response["status"] = 400;
+        $response["error"] = "The type argument was not recognized. $type " .
+            "was provided, but was not one of the following: " .
+            join($defaults["type"], ",") . ".";
+        return $response;
+    }
 
-	$type = strtolower($type);
-	$yesStrict = true;
-	if (!in_array($type, $defaults["type"], $yesStrict)) {
-		$response["status"] = 400;
-		$response["error"] = "The type argument was not recognized. $type was " .
-			"provided, but was not one of the following: " .
-			explode($defaults["type"], ",") . ".";
-		return $response;
-	}
+    $filterTargets = isset($source["filterTargets"]) ?
+        json_decode($source["filterTargets"], true) :
+        null;
+    if (!$filterTargets) {
+        $response["status"] = 400;
+        $response["error"] = "The filterTargets argument was not provided, " .
+            "or was not a valid JSON object.";
+        return $response;
+    } else if (count($filterTargets) === 0) {
+        $response["status"] = 400;
+        $response["error"] = "The filterTargets argument was provided, but " .
+            "was empty.";
+        return $response));
+    }
 
-	$query = isset($_GET["query"]) ? strtolower($_GET["query"]) : null;
-	if ($query === null) {
-		$response["status"] = 400;
-		$response["error"] = "The query argument was not provided.";
-		return $response;
-	}
+    $filterTargets = array_map(function($value) {
+        return strtolower($value);
+    }, $filterTargets);
 
-	$query = strtolower($query);
+    foreach ($filterTargets as $value) {
+        if (!in_array($value, $defaults["filterTargets"], $yesStrict)) {
+            $response["status"] = 400;
+            $response["error"] = "The filterTargets argument was provided, " .
+                "but contained the unknown value $value. Acceptable values " .
+                "are: " . join(", ", $defaults["filterTargets"]) . ".";
+            return $response;
+        }
+    }
 
-	$filterTargets = isset($_GET["filterTargets"]) ?
-		json_decode($_GET["filterTargets"], true) :
-		null;
-	if (!$filterTargets) {
-		$response["status"] = 400;
-		$response["error"] = "The filterTargets argument was not provided, or " .
-			"was not a valid JSON object.";
-		return $response;
-	} else if (count($filterTargets) === 0) {
-		$response["status"] = 400;
-		$response["error"] = "The filterTargets argument was provided, but was " .
-			"empty.";
-		return $response));
-	}
+    $filterStyle = isset($source["filterStyle"]) ?
+        strtolower($source["filterStyle"]) : null;
+    if (!$filterStyle) {
+        $response["status"] = 400;
+        $response["error"] = "The filterStyle argument was not provided.";
+        return $response;
+    } else if (!in_array($filterStyle, $defaults["filterStyles"], $yesStrict)) {
+        $response["status"] = 400;
+        $response["error"] = "The filterStyle argument was provided, but " .
+            "was not one of the following: " .
+            join(", ", $defaults["filterStyles"]) . ".";
+        return $response;
+    }
 
-	$filterTargets = array_map(function($value) {
-		return strtolower($value);
-	}, $filterTargets);
+    $sortTarget = isset($source["sortTarget"]) ?
+        strtolower($source["sortTarget"]) : null;
+    if (!$sortTarget) {
+        $response["status"] = 400;
+        $response["error"] = "The sortTarget argument was not provided.";
+        return $response;
+    } else if (!in_array($sortTarget, $defaults["sortTargets"], $yesStrict)) {
+        $response["status"] = 400;
+        $response["error"] = "The sortTarget argument was provided, but was " .
+            "not one of the following: " .
+            join(", ", $defaults["sortTargets"]) . ".";
+        return $response;
+    }
 
-	foreach ($filterTargets as $value) {
-		if (!in_array($value, $defaults["filterTargets"], $yesStrict)) {
-			$response["status"] = 400;
-			$response["error"] = "The filterTargets argument was provided, but " .
-				"contained the unknown value $value. Acceptable values are: " .
-				join(", ", $allFilterTargets) . ".";
-			return $response;
-		}
-	}
+    $sortStyle = isset($source["sortStyle"]) ?
+        strtolower($source["sortStyle"]) : null;
+    if (!$sortStyle) {
+        $response["status"] = 400;
+        $response["error"] = "The sortStyle argument was not provided.";
+        return $response;
+    } else if (!in_array($sortStyle, $defaults["sortStyles"], $yesStrict)) {
+        $response["status"] = 400;
+        $response["error"] = "The sortStyle argument was provided, but was " .
+            "not one of the following: " .
+            join(", ", $defaults["sortStyles"]) . ".";
+        return $response;
+    }
 
-	$filterStyle = isset($_GET["filterStyle"]) ?
-		strtolower($_GET["filterStyle"]) : null;
-	if (!$filterStyle) {
-		http_response_code(400);
-		$response["status"] = 400;
-		$response["error"] = "The filterStyle argument was not provided.";
-		die(json_encode($response));
-	} else if (!in_array($filterStyle, $defaults["filterStyles"], $yesStrict)) {
-		http_response_code(400);
-		$response["status"] = 400;
-		$response["error"] = "The filterStyle argument was provided, but was " .
-			"not one of the following: " . join(",", $allFilterStyles) . ".";
-		die(json_encode($response));
-	}
+    $sortDirection = isset($source["sortDirection"]) ?
+        $source["sortDirection"] : null;
+    if (!$sortDirection) {
+        $response["status"] = 400;
+        $response["error"] = "The sortDirection argument was not provided.";
+        return $response;
+    } else if (!in_array($sortDirection, $defaults["sortDirection"], $yesStrict)) {
+        $response["status"] = 400;
+        $response["error"] = "The sortDirection argument was provided, but " .
+            "was not one of the following: " .
+            join(", ", $defaults["sortDirection"]) . ".";
+        return $response;
+    }
 
-	$allSortTargets = array(
-		"id",
-		"name",
-		"description",
-		"keywords",
-		"homepage",
-	);
-	$sortTarget = isset($_GET["sortTarget"]) ?
-		strtolower($_GET["sortTarget"]) : null;
-	if (!$sortTarget) {
-		http_response_code(400);
-		$response["status"] = 400;
-		$response["error"] = "The sortTarget argument was not provided.";
-		die(json_encode($response));
-	} else if (!in_array($sortTarget, $allSortTargets)) {
-		http_response_code(400);
-		$response["status"] = 400;
-		$response["error"] = "The sortTarget argument was provided, but was not " .
-			"one of the following: " . join(",", $allSortTargets) . ".";
-		die(json_encode($response));
-	}
+    $dateCreatedRange = isset($source["dateCreatedRange"]) ?
+        $source["dateCreatedRange"] : null;
+    if ($dateCreatedRange) {
+        $dateCreatedRange = json_decode($dateCreatedRange, true);
+        
+        if (!$dateCreatedRange) {
+            http_response_code(400);
+            $response["status"] = 400;
+            $response["error"] = "The dateCreatedRange argument was provided, " .
+                "but it could not be deserialized from JSON.";
+            return $response;
+        } else if (count($dateCreatedRange) !== 2) {
+            $response["status"] = 400;
+            $length = count($dateCreatedRange);
+            $response["error"] = "The dateCreatedRange argument was included, " .
+                "but it contained $length elements, not two.";
+            return $response;
+        } else if (gettype($dateCreatedRange[0]) !== "integer" or
+            gettype($dateCreatedRange[1]) !== "integer")
+        {
+            $response["status"] = 400;
+            $response["error"] = "The dateCreatedRange argument was provided, " .
+                "but one or both array items is not an integer.";
+            return $response;
+        } else if (!($dateCreatedRange[0] >= 0 and
+            $dateCreatedRange[1] >= $dateCreatedRange[0]))
+        {
+            $response["status"] = 400;
+            $response["error"] = "The dateCreatedRange argument was included, " .
+                "and had two elements, but it did not contain two integers, the " .
+                "first greater than or equal to 0, and the second greater than " .
+                "or equal the first.";
+            return $response;
+        }
+    }
 
-	$allSortStyles = array(
-		"alphanumeric",
-		"similarity",
-		"levenshtein",
-		"soundex/levenshtein",
-		"metaphone/levenshtein",
-	);
-	$sortStyle = isset($_GET["sortStyle"]) ?
-		strtolower($_GET["sortStyle"]) : null;
-	if (!$sortStyle) {
-		http_response_code(400);
-		$response["status"] = 400;
-		$response["error"] = "The sortStyle argument was not provided.";
-		die(json_encode($response));
-	} else if (!in_array($sortStyle, $allSortStyles)) {
-		http_response_code(400);
-		$response["status"] = 400;
-		$response["error"] = "The sortStyle argument was provided, but was not " .
-			"one of the following: " . join(",", $allSortStyles) . ".";
-		die(json_encode($response));
-	}
+    $dateModifiedRange = isset($source["dateModifiedRange"]) ?
+        $source["dateModifiedRange"] : null;
+    if ($dateModifiedRange) {
+        $dateModifiedRange = json_decode($dateModifiedRange, true);
+        if (!$dateModifiedRange) {
+            $response["status"] = 400;
+            $response["error"] = "The dateModifiedRange argument was provided, " .
+                "but it could not be deserialized from JSON.";
+            return $response;
+        } else if (count($dateModifiedRange) !== 2) {
+            $response["status"] = 400;
+            $length = count($dateModifiedRange);
+            $response["error"] = "The dateModifiedRange argument was included, " .
+                "but it contained $length elements, not two.";
+            return $response;
+        } else if (gettype($dateModifiedRange[0]) !== "integer" or
+            gettype($dateModifiedRange[1]) !== "integer")
+        {
+            $response["status"] = 400;
+            $response["error"] = "The dateModifiedRange argument was provided, " .
+                "but one or both array items is not an integer.";
+            return $response;
+        } else if (!($dateModifiedRange[0] >= 0 and
+            $dateModifiedRange[1] >= $dateModifiedRange[0]))
+        {
+            $response["status"] = 400;
+            $response["error"] = "The dateModifiedRange argument was included, " .
+                "and had two elements, but it did not contain two integers, the " .
+                "first greater than or equal to 0, and the second greater than " .
+                "or equal the first.";
+            return $response;
+        }
+    }
 
-	$sortDirection = isset($_GET["sortDirection"]) ? $_GET["sortDirection"] : null;
-	if (!$sortDirection) {
-		http_response_code(400);
-		$response["status"] = 400;
-		$response["error"] = "The sortDirection argument was not provided.";
-		die(json_encode($response));
-	} else if ($sortDirection !== "asc" and
-		$sortDirection !== "ascending" and
-		$sortDirection !== "desc" and
-		$sortDirection !== "descending")
-	{
-		http_response_code(400);
-		$response["status"] = 400;
-		$response["error"] = "The sortDirection argument was provided, but was " .
-			"asc, ascending, desc, or descending.";
-		die(json_encode($response));
-	}
+    $versionRange = isset($source["versionRange"]) ?
+        $source["versionRange"] : null;
+    if ($versionRange) {
+        $versionRange = json_decode($versionRange, true);
 
-	$dateCreatedRange = isset($_GET["dateCreatedRange"]) ?
-		$_GET["dateCreatedRange"] : null;
-	if ($dateCreatedRange) {
-		$dateCreatedRange = json_decode($dateCreatedRange, true);
-		
-		if (!$dateCreatedRange) {
-			http_response_code(400);
-			$response["status"] = 400;
-			$response["error"] = "The dateCreatedRange argument was provided, " .
-				"but it could not be deserialized from JSON.";
-			die(json_encode($response));
-		} else if (count($dateCreatedRange) !== 2) {
-			http_response_code(400);
-			$response["status"] = 400;
-			$length = count($dateCreatedRange);
-			$response["error"] = "The dateCreatedRange argument was included, " .
-				"but it contained $length elements, not two.";
-			die(json_encode($response));
-		} else if (gettype($dateCreatedRange[0]) !== "integer" or
-			gettype($dateCreatedRange[1]) !== "integer")
-		{
-			http_response_code(400);
-			$response["status"] = 400;
-			$response["error"] = "The dateCreatedRange argument was provided, " .
-				"but one or both array items is not an integer.";
-		} else if (!($dateCreatedRange[0] >= 0 and
-			$dateCreatedRange[1] >= $dateCreatedRange[0]))
-		{
-			http_response_code(400);
-			$response["status"] = 400;
-			$response["error"] = "The dateCreatedRange argument was included, " .
-				"and had two elements, but it did not contain two integers, the " .
-				"first greater than or equal to 0, and the second greater than " .
-				"or equal the first.";
-			die(json_encode($response));
-		}
-	}
+        if (!$versionRange) {
+            $response["status"] = 400;
+            $response["error"] = "The versionRange argument was provided, but it " .
+                "could not be deserialized from JSON.";
+            return $response;
+        } else if (count($versionRange) !== 2) {
+            $response["status"] = 400;
+            $response["error"] = "The versionRange argument was included, but it " .
+                "did not contain only two elements.";
+            return $response;
+        } else if (gettype($versionRange[0]) !== "string" or
+            gettype($versionRange[1]) !== "string")
+        {
+            $response["status"] = 400;
+            $response["error"] = "The versionRange argument was provided, " .
+                "but one or both array items is not a string.";
+            return $response;
+        } else if (!($versionRange[1] >= $versionRange[0])) {
+            $response["status"] = 400;
+            $response["error"] = "The versionRange argument was included, " .
+                "and had two elements, but it did not contain two strings, " .
+                "the second greater than or equal the first.";
+            return $response;
+        }
+    }
 
-	$dateModifiedRange = isset($_GET["dateModifiedRange"]) ?
-		$_GET["dateModifiedRange"] : null;
-	if ($dateModifiedRange) {
-		$dateModifiedRange = json_decode($dateModifiedRange, true);
+    $subtype = isset($source["subtype"]) ? $source["subtype"] : null;
 
-		if (!$dateModifiedRange) {
-			http_response_code(400);
-			$response["status"] = 400;
-			$response["error"] = "The dateModifiedRange argument was provided, " .
-				"but it could not be deserialized from JSON.";
-			die(json_encode($response));
-		} else if (count($dateModifiedRange) !== 2) {
-			http_response_code(400);
-			$response["status"] = 400;
-			$length = count($dateModifiedRange);
-			$response["error"] = "The dateModifiedRange argument was included, " .
-				"but it contained $length elements, not two.";
-			die(json_encode($response));
-		} else if (gettype($dateModifiedRange[0]) !== "integer" or
-			gettype($dateModifiedRange[1]) !== "integer")
-		{
-			http_response_code(400);
-			$response["status"] = 400;
-			$response["error"] = "The dateModifiedRange argument was provided, " .
-				"but one or both array items is not an integer.";
-			die(json_encode($response));
-		} else if (!($dateModifiedRange[0] >= 0 and
-			$dateModifiedRange[1] >= $dateModifiedRange[0]))
-		{
-			http_response_code(400);
-			$response["status"] = 400;
-			$response["error"] = "The dateModifiedRange argument was included, " .
-				"and had two elements, but it did not contain two integers, the " .
-				"first greater than or equal to 0, and the second greater than " .
-				"or equal the first.";
-			die(json_encode($response));
-		}
-	}
+    $params = array(
+        "query" => $query,
+        "type" => $type,
+        "filterTargets" => $filterTargets,
+        "filterStyle" => $filterStyle,
+        "sortTarget" => $sortTarget,
+        "sortStyle" => $sortStyle,
+        "sortDirection" => $sortDirection,
+        "dateCreatedRange" => $dateCreatedRange,
+        "dateModifiedRange" => $dateModifiedRange,
+        "versionRange" => $versionRange,
+        "subtype" => $subtype
+    );
 
-	$versionRange = isset($_GET["versionRange"]) ?
-		$_GET["versionRange"] : null;
-	if ($versionRange) {
-		$versionRange = json_decode($versionRange, true);
+    $response["params"] = $params;
 
-		if (!$versionRange) {
-			$response["status"] = 400;
-			$response["error"] = "The versionRange argument was provided, but it " .
-				"could not be deserialized from JSON.";
-			return $response;
-		} else if (count($versionRange) !== 2) {
-			http_response_code(400);
-			$response["status"] = 400;
-			$response["error"] = "The versionRange argument was included, but it " .
-				"did not contain only two elements.";
-			die(json_encode($response));
-		} else if (gettype($versionRange[0]) !== "string" or
-			gettype($versionRange[1]) !== "string")
-		{
-			http_response_code(400);
-			$response["status"] = 400;
-			$response["error"] = "The versionRange argument was provided, " .
-				"but one or both array items is not a string.";
-			die(json_encode($response));
-		} else if (!($versionRange[1] >= $versionRange[0])) {
-			http_response_code(400);
-			$response["status"] = 400;
-			$response["error"] = "The versionRange argument was included, and " .
-				"had two elements, but it did not contain two strings, the " .
-				"second greater than or equal the first.";
-			die(json_encode($response));
-		}
-	}
-
-	$subtype = isset($_GET["subtype"]) ? $_GET["subtype"] : null;
-
-	return $response;
+    return $response;
 ?>
