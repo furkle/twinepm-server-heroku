31779cf4ca7b95f1af10cdd5d794a7a2fab876f5
furkle <furkle@furkleindustries.com>
2017-09-16 17:08:26 -0400

    Fixing REDIS_URL.

--- logic/Getters/RedisServerGetter.php
+++ logic/Getters/RedisServerGetter.php
@@ -4,7 +4,6 @@ namespace TwinePM\Getters;
 use \Predis\Client;
 class RedisServerGetter implements IGetter {
     public static function get(array $context = null): Client {
-        $server = new Client(RedisServerUrlGetter::get());
-        return $server;
+        return new Client(RedisServerUrlGetter::get());
     }
 }
